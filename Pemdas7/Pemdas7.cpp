f654560a8201432b8779f3febf28a31086798a84
verss21 <arjunafrizal01@gmail.com>
2024-04-22 14:15:55 +0700

    void output

--- Pemdas7/Pemdas7.cpp
+++ Pemdas7/Pemdas7.cpp
@@ -6,4 +6,11 @@ public: // akses modifier
 	string nama;
 	int umur;
 	string jurusan
-}
+
+		void output() {
+		cout << "Nama: " << nama << endl;
+		cout << "Umur: " << umur << endl;
+		cout << "Jurusan" << jurusan << endl;
+	}
+
+};
